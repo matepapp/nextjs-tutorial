ce3083f293f06aa9dc3e2c7b60affa9897206c86
Mate Papp <matepapp@icloud.com>
2020-01-28 11:06:06 +0100

    API routes

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,48 @@
-import fetch from "isomorphic-unfetch";
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 
-const ShowLink = ({ show }) => (
-  <li key={show.id}>
-    <Link href="/p/[id]" as={`/p/${show.id}`}>
-      <a>{show.name}</a>
-    </Link>
-    <style jsx>
-      {`
-        li {
-          list-style: none;
-          margin: 5px 0;
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
+
+export default function Index() {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? "?author=" + query.author : ""}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
+
+  if (!data) quote = "Loading...";
+  if (error) quote = "Failed to fetch the quote.";
+
+  return (
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
+
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
         }
-
-        a {
-          text-decoration: none;
-          color: blue;
-          font-family: "Arial";
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
         }
-
-        a:hover {
-          opacity: 0.6;
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
         }
-      `}
-    </style>
-  </li>
-);
-
-const Index = props => (
-  <Layout>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(show => (
-        <ShowLink show={show} />
-      ))}
-    </ul>
-    <style jsx>{`
-      h1,
-      a {
-        font-family: "Arial";
-      }
-
-      ul {
-        padding: 0;
-      }
-    `}</style>
-  </Layout>
-);
-
-Index.getInitialProps = async function() {
-  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
-  const data = await res.json();
-
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data.map(entry => entry.show)
-  };
-};
-
-export default Index;
+      `}</style>
+    </main>
+  );
+}
